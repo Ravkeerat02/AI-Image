789bb419f2a3355f78a0318884fb15f86d759fe9
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-09-22 16:16:54 -0400

    NOT WORKING

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,21 +1,23 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { OpenAI } from "openai";
+import OpenAI from "openai";
 
 dotenv.config();
 
 const router = express.Router();
+
 const openai = new OpenAI({
-  apiKey: process.env.OPEN_AI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 router.get("/", (req, res) => {
-  res.send("Hello");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
 router.post("/", async (req, res) => {
   try {
     const { prompt } = req.body;
+
     const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
@@ -23,22 +25,19 @@ router.post("/", async (req, res) => {
       response_format: "b64_json",
     });
 
-    if (
-      aiResponse.data &&
-      aiResponse.data.data &&
-      aiResponse.data.data.length > 0
-    ) {
-      const image = aiResponse.data.data[0].b64_json;
+    const images = aiResponse.data.images;
+    if (images && images.length > 0) {
+      const image = images[0].b64_json;
       res.status(200).json({ photo: image });
     } else {
-      console.error("Unexpected or empty response:", aiResponse);
-      res
-        .status(500)
-        .send("Unexpected or empty response from image generation");
+      console.error("No image found in the response:", aiResponse);
+      res.status(500).send("No image found in the response");
     }
   } catch (error) {
     console.error("Error generating image:", error);
-    res.status(500).send("An error occurred while generating image");
+    res
+      .status(500)
+      .send(error?.response?.data?.error?.message || "Something went wrong");
   }
 });
 
